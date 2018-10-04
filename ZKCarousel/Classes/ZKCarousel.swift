9314b740b4f20f4ca339f703ea572430aa2c8fbb
Ariel Rodriguez <ariel@volonbolon.me>
2018-10-04 17:17:29 -0300

    Remove addBlackGradientLayer

--- ZKCarousel/Classes/ZKCarousel.swift
+++ ZKCarousel/Classes/ZKCarousel.swift
@@ -209,9 +209,7 @@ final public class ZKCarouselSlide : NSObject {
 }
 
 extension UIView {
-
     func addConstraintsWithFormat(_ format: String, views: UIView...) {
-
         var viewsDictionary = [String: UIView]()
         for (index, view) in views.enumerated() {
             let key = "v\(index)"
@@ -220,17 +218,5 @@ extension UIView {
         }
 
         addConstraints(NSLayoutConstraint.constraints(withVisualFormat: format, options: NSLayoutFormatOptions(), metrics: nil, views: viewsDictionary))
-    }
-
-
-}
-
-extension UIImageView {
-    func addBlackGradientLayer(frame: CGRect){
-        let gradient = CAGradientLayer()
-        gradient.frame = frame
-        gradient.colors = [UIColor.clear.cgColor, UIColor.black.withAlphaComponent(0.8).cgColor]
-        gradient.locations = [0.0, 0.6]
-        self.layer.insertSublayer(gradient, at: 0)
     }
 }
